23aa76c53e1ce51b2620575930ff87a2cb7079ff
maecapozzi <maecapozzi@gmail.com>
2016-01-29 16:12:53 -0500

    include jquery

---
+++ content.js
@@ -0,0 +1,22 @@
+// highlights the word sorry on the page. 
+
+// searches the entire document 
+// if it finds the word that matches "sorry", "Sorry", or "SORRY", it highlights that word
+
+//when the button is clicked, needs to take all of the text in the div
+//put that text into an array 
+// loop through all of the words 
+//check to see if a word matches "sorry". 
+// If the word matches, highlight the word. 
+
+
+
+var doc = document.getElementsByClassName("Am");
+debugger
+// var text = doc[0].innerHTML.split(" "); 
+
+$(".z0").click(function(){ 
+
+
+
+});
