ebce019f4bfda262319b58712edc2b67bde25e58
maecapozzi <maecapozzi@gmail.com>
2016-01-29 14:11:17 -0500

    creates an array of all of the text

---
+++ popup.js
@@ -0,0 +1,21 @@
+// highlights the word sorry on the page. 
+
+// searches the entire document 
+// if it finds the word that matches "sorry", "Sorry", or "SORRY", it highlights that word
+
+//loop through all of the words
+// if a word matches "sorry", highlight it
+
+
+(function() {
+   // your page initialization code here
+   // the DOM will be available here
+
+var doc = document.getElementsByClassName("Am");
+
+var text = doc[0].innerHTML;
+
+var textArray = text.split(" "); 
+
+})();
+
